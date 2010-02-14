3815e45b899a2da42b74eeb478c0aaac343545e4
darren.j.white <darren.j.white@b1f88d2a-1737-11df-85d1-b5325210c051>
2010-02-14 05:30:46 +0000

    Updated headers.

--- Headers/database.hpp
+++ Headers/database.hpp
@@ -7,6 +7,9 @@
 #include "relation.hpp"
 #include "parser.hpp"
 
+// Forward Declaration
+class Parser;
+
 using namespace std;
 
 class Database{
@@ -14,17 +17,16 @@ public:
     Database();   // Constructor
     ~Database();  // Deconstructor
     
-    Relation execute(string command);                  // Parses command query to create a new relation
-    Relation execute(string name, string query);       // Pulls relation "name", parses the query, and performs operations.
+    void execute(string command);                  // Parses command query to create a new relation
     
     //void execute(string name, string query, back_inserter);  // Not sure what the return type of this should be  
     
     void add_relation(Relation r);                     // Adds to currently managed relations vector
-    Relation get_relation(string name);                // Goes through relations vector and pulls out the one requested
+    Relation * get_relation(string name);              // Goes through relations vector and pulls out the one requested
     bool save(Relation r);                             // Writes given relation to a text file of the same name, (warn/overwrite if already exists?)
     bool load(string name);                            // Grabs relation from file, puts into relations vector, returns true if successful
 private:
-    Parser p;
+    Parser *parser;
     vector<Relation> relations;  // All the relations database is handling currently, perhaps we should limit its size?
 };
 
